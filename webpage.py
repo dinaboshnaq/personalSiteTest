69bcbe2a89e09fedb01842ffe2a10e4c44a73195
dinaboshnaq <dina.boshnaq2@gmail.com>
2018-07-21 12:19:30 +0300

    These are the files needed

---
+++ webpage.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from random import randint
+
+app=Flask(__name__)
+@app.route('/intro')
+def loadPage():
+	return render_template('intro.html')
+
+
+@app.route('/intro2')
+def loadPage2():
+	return render_template('intro2.html')
+
+
+if __name__=='__main__':
+	app.run(debug=True, port=1610)
